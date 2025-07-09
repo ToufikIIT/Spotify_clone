a5e1263ad90e5c25a034be096414ebcf4227b5e1
ToufikIIT <toufik.haque2021@gmail.com>
2025-07-09 12:49:48 +0530

    spotify

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getsongs(folder) {
     currFolder = folder;
-    let a = await fetch(`songs/${folder}/`)
+    let a = await fetch(`${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
